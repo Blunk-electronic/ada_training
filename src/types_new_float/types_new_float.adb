84bb3a2c4559953a65deffef8cb2784a1fcf5f5d
Mario Blunk <mario.blunk@blunk-electronic.de>
2016-12-24 21:20:34 +0100

    added user specific float type

---
+++ src/types_new_float/types_new_float.adb
@@ -0,0 +1,22 @@
+-- This is a simple ada program,
+-- that demonstrates a user specific 
+-- new type float with a given precision.
+
+with ada.text_io;	use ada.text_io;
+
+procedure types_new_float is
+
+	type kilogramms is digits 4 range 0.0 .. 11.0;
+	mass : kilogramms := 0.2;
+
+begin
+	-- mass := 11.4; -- Causes a warning at compile and
+					 -- a constraint error at run time.
+
+	mass := 10.035;  -- discards the last digit (5).
+
+	put_line("the apple weights :" & kilogramms'image(mass) & " kg");
+
+	mass := mass / 2.0; -- discards the last digit (5).
+	put_line("the apple half weights :" & kilogramms'image(mass) & " kg");
+end types_new_float;
