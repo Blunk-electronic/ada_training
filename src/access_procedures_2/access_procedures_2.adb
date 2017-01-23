182dfe29698e179a8b8aca8eb35fb4112df6adae
Mario Blunk <mario.blunk@blunk-electronic.de>
2017-01-23 21:04:28 +0100

    wrote more comments

--- src/access_procedures_2/access_procedures_2.adb
+++ src/access_procedures_2/access_procedures_2.adb
@@ -8,21 +8,26 @@ procedure access_procedures_2 is
 	-- Define an access to ANY procedure that "inouts" a number:
 	type type_my_access is not null access procedure (n : in out integer);
 
+	-- This is a procedure that doubles a number:
 	procedure double (x : in out integer) is begin
 		x := x * 2;
 	end double;
 
+	-- This is a procedure that squares a number:
 	procedure square (y : in out integer) is begin
 		y := y ** 2;
 	end square;
 
 	a : integer := 3;
+
+	-- P "points" to procedure "double":
 	p : type_my_access := double'access;
 
 begin
+	p(a);	put_line(integer'image(a)); -- result 6
 
-	p(a);	put_line(integer'image(a));
- 	p := square'access;
-	p(a);	put_line(integer'image(a));
+	-- p "points" to procedure "square":
+ 	p := square'access; 
+	p(a);	put_line(integer'image(a)); -- result 36
 
 end access_procedures_2;
