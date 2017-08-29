a3817e7aaee0d09f367d0dad7c2b2a67c0114d04
Mario Blunk <mario.blunk@blunk-electronic.de>
2017-08-29 21:46:43 +0200

    how to customize float types

---
+++ src/types_float/types_float.adb
@@ -0,0 +1,27 @@
+-- This is a simple ada program,
+-- that demonstrates how to customize
+-- float types.
+
+with ada.text_io;	use ada.text_io;
+
+procedure types_float is
+
+	f0 : float := 5.5; 	-- accuracy machine depended
+						-- you get any accuracy
+						-- probably too much accuracy
+
+	-- well defined accuracy
+	type float_1 is digits 2;	
+	f1 : float_1 := 5.5;
+
+	-- well defined range
+	type float_2 is digits 2 range 0.0 .. 2.0;
+	f2 : float_2 := 1.1;
+	
+begin
+
+	put_line (float'image (f0));	-- 5.50000E+00
+	put_line (float_1'image (f1));	-- 5.5E+00
+	put_line (float_2'image (f2));	-- 1.1E+00
+	
+end types_float;
