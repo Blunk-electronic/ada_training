f0f109d3364e7be5e7954639af9e72a0fec40240
Mario Blunk <mario.blunk@blunk-electronic.de>
2018-11-13 09:00:17 +0100

    example on fixed point

---
+++ src/types_fixed_point_1/types_fixed_point_1.adb
@@ -0,0 +1,27 @@
+-- This is a simple ada program,
+-- that demonstrates fixed point types.
+
+with ada.text_io;	use ada.text_io;
+
+procedure types_fixed_point_1 is
+
+	step_width : constant := 0.01;
+	type type_angle is delta step_width range 0.0 .. 359.99;
+	for type_angle'small use step_width;
+	
+	angle : type_angle;
+	
+begin
+	put_line ("step width :" & type_angle'image (step_width));
+	
+	angle := type_angle'last; -- 359.99
+	angle := type_angle'first; -- 0.01
+
+	for i in 1..10 loop
+		
+		angle := angle + type_angle'small;
+		put_line ("angle :" & type_angle'image (angle));
+		
+	end loop;
+		
+end types_fixed_point_1;
