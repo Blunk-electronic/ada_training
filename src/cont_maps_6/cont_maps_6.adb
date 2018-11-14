d0f1f5e6b357e5ec957f8d40c173d6cfee584529
Mario Blunk <mario.blunk@blunk-electronic.de>
2018-11-14 15:03:19 +0100

    query items in map

---
+++ src/cont_maps_6/cont_maps_6.adb
@@ -0,0 +1,67 @@
+-- This is a simple ada program, that
+-- demonstrates a map.
+
+with ada.text_io; use ada.text_io;
+with ada.containers; use ada.containers;
+with ada.containers.ordered_maps;
+with ada.containers.doubly_linked_lists;
+
+procedure cont_maps_6 is
+
+  package my_list is new ada.containers.doubly_linked_lists(natural);
+  use my_list;
+
+	package type_my_map is new ordered_maps (
+		key_type 		=> character,
+		element_type	=> my_list.list);
+	
+	use type_my_map;
+
+ 	m : type_my_map.map;
+	c : type_my_map.cursor;
+	
+	inserted : boolean; -- goes true if the object got inserted
+
+	procedure change (
+		name	: in character;
+		thing	: in out list) is
+	begin
+		thing.append(100);
+		thing.append(101);
+	end change;
+
+	procedure query (
+		name	: in character;
+		thing	: in list) is
+
+		procedure print_item (cursor : my_list.list.cursor) is
+		begin	
+			put_line (natural'image (element (cursor)));
+		end print_item;
+
+	begin -- query
+		thing.iterate (print_item'access);
+	end query;
+
+begin -- cont_maps_6
+	insert (  -- insert object '7' with key 'A'
+		container	=> m,
+		key			=> 'A',
+		position	=> c,
+		inserted	=> inserted
+		);
+
+	if inserted then
+		update_element (
+			container	=> m,
+			position	=> c,
+			process		=> change'access);
+	end if;
+
+	--put_line (natural'image ( element (c).first_element));
+	query_element (
+		container	=> m,
+		position	=> c,
+		process		=> query'access);
+
+end cont_maps_6;
