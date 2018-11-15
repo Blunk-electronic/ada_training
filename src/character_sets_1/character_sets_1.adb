3fd8e3e48165c0f0ff9831ce25e293d6bcee95e8
Mario Blunk <mario.blunk@blunk-electronic.de>
2018-11-15 15:21:55 +0100

    character test ready

--- src/character_sets_1/character_sets_1.adb
+++ src/character_sets_1/character_sets_1.adb
@@ -1,19 +1,28 @@
 -- This is a simple ada program, that demonstrates character sets.
 
-with ada.text_io;	
+with ada.text_io;				use ada.text_io;
+with ada.strings; 				use ada.strings;
 with ada.strings.maps;			use ada.strings.maps;
+with ada.strings.bounded;       use ada.strings.bounded;
 
 procedure character_sets_1 is
 
+	package type_string is new generic_bounded_length (100);
+	use type_string;
+	bs : type_string.bounded_string := to_bounded_string ("AB-C");
+	
 	char_set : character_set := to_set (span => ('A','Z')); 
 
 	invalid_character_position : natural := 0;
 begin
 
-	begin
-		invalid_character_position := index (
-			source 	=> bounded_string,
-			set 	=> char_set,
-			test 	=> outside);
-		
+	invalid_character_position := index (
+		source 	=> bs,
+		set 	=> char_set,
+		test 	=> outside);
+
+	if invalid_character_position > 0 then
+		put_line ("invalid character at position" 
+			& natural'image (invalid_character_position) & " !");
+	end if;
 end character_sets_1;
