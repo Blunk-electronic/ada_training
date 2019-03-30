b9a92c3d38323a87903b0e899f6d3b50c53cc919
Mario Blunk <mario.blunk@blunk-electronic.de>
2019-03-30 21:36:39 +0100

    rework

--- src/type_angle/type_angle.adb
+++ src/type_angle/type_angle.adb
@@ -1,18 +1,30 @@
 -- This is a simple ada program,
 -- demonstrates some data types.
 
--- with ada.text_io;	use ada.text_io;
+pragma assertion_policy (check);
+with ada.text_io;	use ada.text_io;
 
 procedure type_angle is
-	Number_Small : constant := 5.0;
-	type Number is delta Number_Small range -100.0 .. 100.0;
-	for Number'Small use Number_Small;
-
-	angle : number := 101.0;
+	subtype angle is integer range -355 .. 355
+	with dynamic_predicate => angle mod 5 = 0;
+	a : angle;
 begin
-	-- no warning and no error at runtime:
-	angle := 101.0; -- expect an error at compile time
+	a := -355;
+	a :=  355;
+	-- a := 360; -- warning at compile time, error at runtime -> ok
+	a := 0;
+	a := 5;
+	
+	-- a := 7; -- error at compile time -> ok
 
-	-- warning at compile time and constraint error at runtime:
-	angle := 110.0; -- expect an error at compile time
+	a := a + 2; -- error at runtime -> ok
+
+	exception 
+		when constraint_error => 
+			put_line ("outside range");
+
+		--when SYSTEM.ASSERTIONS.ASSERT_FAILURE => -- ?
+		when others =>
+			put_line ("invalid step width ! Step width must be " & "5"); -- ?
+
 end type_angle;
