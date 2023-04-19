76c607d2e177d1689e4d9b1052b7ecc707ebf850
Mario Blunk <mario.blunk@blunk-electronic.de>
2023-04-19 08:56:15 +0200

    complete demo

--- src/containers/demo/nested/nest_1.adb
+++ src/containers/demo/nested/nest_1.adb
@@ -1,3 +1,6 @@
+-- This program demonstrates how to search items
+-- in nested containers.
+
 with ada.text_io; 			use ada.text_io;
 with ada.containers;		use ada.containers;
 with ada.containers.ordered_maps;
@@ -10,6 +13,10 @@ procedure nest_1 is
 		S, E : float := 0.0;
 	end record;
 
+	function to_string (l : in type_line) return string is begin
+		return "S:" & float'image (l.S) & " E:" & float'image (l.E);
+	end;
+	
 	package pac_lines is new doubly_linked_lists (type_line);
 	use pac_lines;
 	----------------------------
@@ -30,14 +37,61 @@ procedure nest_1 is
 	use pac_nets;
 	
 	nets : pac_nets.map;
+	------------------------------
 
+	-- This procedure searches for a line that starts at 2.0.
+	-- The result is a cursor to the affected line.
+	procedure get_line is 
+		result : pac_lines.cursor;
+		
+		procedure query_net (n : in pac_nets.cursor) is
+			net : type_net renames element (n);
+			-- net : type_net := element (n);
+
+			procedure query_line (l : in pac_lines.cursor) is begin
+				-- Test the start point of the candidate line.
+				-- On match, store its cursor in result:
+				if element (l).S = 2.0 then -- match
+					result := l;
+					put_line ("L1 " & to_string (element (result)));
+				end if;
+			end query_line;
+			
+		begin
+			-- iterate the lines of the candidate net:
+			put_line ("net: " & to_string (key (n)));
+			iterate (net.lines, query_line'access);
+			-- iterate (element (n).lines, query_line'access);
+
+			-- Output the result. 
+			-- It should never change once the targeted line has been found:
+			put_line ("L2 " & to_string (element (result)));
+		end query_net;
+		
+	begin
+		-- iterate the nets:
+		nets.iterate (query_net'access);
+
+		-- Output the result. 
+		-- It should never change once the targeted line has been found:
+		put_line ("L3 " & to_string (element (result)));
+	end get_line;
+		
+	
 
 begin
+	-- build net A:
 	net.lines.append ((0.0, 1.0));
 	net.lines.append ((2.0, 3.0));
 	net.lines.append ((5.0, 6.0));
-
 	nets.insert (to_bounded_string ("A"), net);
 
+	-- build net B:
+	net.lines.clear;
+	net.lines.append ((10.0, 11.0));
+	net.lines.append ((12.0, 13.0));
+	net.lines.append ((15.0, 16.0));
+	nets.insert (to_bounded_string ("B"), net);
 
+	get_line;
 end nest_1;
