e652bc63c5c8c7c8dadacca2303020d3ad988af1
Mario Blunk <mario.blunk@blunk-electronic.de>
2023-05-22 11:01:51 +0200

    example of canvas inside a scrolled window

---
+++ src/gtk/cairo_2/draw.adb
@@ -0,0 +1,88 @@
+-- This program demonstrates a drawing area - a so called 
+-- canvas (german: Leinwand) inside a scrolled window.
+-- It draws a red rectangle in the upper left corner
+-- as shown in image file draw.jpg.
+
+-- build with command "gprbuild"
+-- clean up with command "gprclean"
+
+
+with gtk.main;					use gtk.main;
+with gtk.widget;				use gtk.widget;
+with gtk.adjustment;			use gtk.adjustment;
+with gtk.window;				use gtk.window;
+with gtk.enums;					use gtk.enums;
+with gtk.scrolled_window;		use gtk.scrolled_window;
+with gtk.drawing_area;			use gtk.drawing_area;
+with gtk.box;					use gtk.box;
+
+with ada.text_io;				use ada.text_io;
+with callbacks;					use callbacks;
+
+
+procedure draw is
+
+	window		: gtk_window;
+	swin		: gtk_scrolled_window;
+	canvas		: gtk_drawing_area;
+
+	horizontal, vertical : gtk_adjustment;
+begin
+	init;
+
+	-- Set up the main window::
+	window := gtk_window_new (WINDOW_TOPLEVEL);
+	window.set_title ("Canvas");
+	window.set_border_width (10);
+	-- window.set_size_request (300, 200);
+	window.set_default_size (300, 200);
+	window.on_destroy (cb_terminate'access);
+
+
+	
+	
+	-- Create a scrolled window:
+	swin := gtk_scrolled_window_new (hadjustment => null, vadjustment => null);
+	horizontal := swin.get_hadjustment;
+	vertical := swin.get_vadjustment;
+
+	-- Connect the signal "value-changed" of the scrollbars with 
+	-- procedures cb_vertical_moved and cb_horizontal_moved. So the user
+	-- can watch how the signals are emitted:
+	vertical.on_value_changed (cb_vertical_moved'access);
+	horizontal.on_value_changed (cb_horizontal_moved'access);
+	
+	swin.set_border_width (5);
+
+	swin.set_policy ( -- for scrollbars
+		hscrollbar_policy => gtk.enums.POLICY_AUTOMATIC, 
+		vscrollbar_policy => gtk.enums.POLICY_AUTOMATIC);
+
+
+	
+	-- Set up the drawing area:
+	gtk_new (canvas);
+	-- canvas.add_events (gdk.event.button_press_mask);
+
+	-- Set the size of the area required for all the objects
+	-- to be drawn. This is the so called "bounding rectangle"
+	-- around everything to be drawn. The size of the bounding
+	-- rectangle MUST be known beforehand of calling the
+	-- callback procedure cb_draw (see below):
+	canvas.set_size_request (600, 200); -- unit is pixels
+	
+	canvas.on_draw (cb_draw'access);
+	-- NOTE: No context is declared here, because the canvas widget
+	-- passes its own context to the callback procedure cb_draw.
+
+	
+	-- Add the canvas as a child to the scrolled window:
+	swin.add_with_viewport (canvas); 
+
+	-- Add the scrolled window as a child to the main window:
+	window.add (swin);
+
+	window.show_all;
+	gtk.main.main;
+end draw;
+
---
+++ src/gtk/cairo_2/lib/callbacks.adb
@@ -0,0 +1,52 @@
+with ada.text_io;				use ada.text_io;
+with ada.calendar;				use ada.calendar;
+with ada.calendar.formatting;	use ada.calendar.formatting;
+
+with gtk.main;					use gtk.main;
+
+
+package body callbacks is
+
+	procedure cb_terminate (
+		main_window : access gtk_widget_record'class) 
+	is begin
+		put_line ("exiting ...");
+		gtk.main.main_quit;
+	end cb_terminate;
+
+
+	procedure cb_horizontal_moved (
+		scrollbar : access gtk_adjustment_record'class)
+	is begin
+		put_line ("horizontal moved " & image (clock));
+	end cb_horizontal_moved;
+
+	
+	procedure cb_vertical_moved (
+		scrollbar : access gtk_adjustment_record'class)
+	is begin
+		put_line ("vertical moved " & image (clock));
+	end cb_vertical_moved;
+
+	
+	function cb_draw (
+		canvas	: access gtk_widget_record'class;
+		context	: in cairo_context)
+		return boolean
+	is
+		result : boolean := true;
+	begin
+		put_line ("cb_draw " & image (clock));
+
+		set_line_width (context, 1.0);
+		set_source_rgb (context, 1.0, 0.0, 0.0);
+
+		rectangle (context, 1.0, 1.0, 500.0, 100.0);
+		stroke (context);
+		
+		return result;
+	end cb_draw;
+
+	
+end callbacks;
+
