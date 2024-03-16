4463e54bc272fc07feb596b5c7b8f683b54cf6f3
Mario Blunk <mario.blunk@blunk-electronic.de>
2024-03-16 18:52:12 +0100

    rework example on decimal fixed point

--- src/fixed_point_numbers/decimal_1/demo.adb
+++ src/fixed_point_numbers/decimal_1/demo.adb
@@ -1,9 +1,10 @@
 -- This is a simple ada program,
--- that demonstrates a decimal fixed point types.
+-- that demonstrates a so called 
+-- decimal fixed point type.
 
 with ada.text_io;	use ada.text_io;
 
-procedure types_fixed_point_4 is
+procedure demo is
 
 	step_width : constant := 0.01;
 	type type_distance is delta step_width digits 8 range 0.0 .. 100_000.00;
@@ -12,12 +13,12 @@ procedure types_fixed_point_4 is
 	
 begin
 	-- Each iteration of this loop adds 0.01 to the distance.
-	-- We expect an outcome of 1000.00:
+	-- We expect an outcome of 1,000.00:
 	
 	for i in 1 .. 100_000 loop
-		distance := distance + 0.01;		
+		distance := distance + step_width;		
 	end loop;
 
-	put_line (type_distance'image (distance)); -- 1000.00
+	put_line (type_distance'image (distance)); -- 1,000.00
 	
-end types_fixed_point_4;
+end demo;
