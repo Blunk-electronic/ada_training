ca1e54094482507846fe85aece7269984b701d6b
Mario Blunk <mario.blunk@blunk-electronic.de>
2026-04-08 17:15:39 +0200

    clean up

--- src/containers/vectors/cont_vectors_1/cont_vectors_1.adb
+++ src/containers/vectors/cont_vectors_1/cont_vectors_1.adb
@@ -7,17 +7,17 @@ with ada.containers.vectors;
 
 procedure cont_vectors_1 is
 
-	package type_my_vector is new 
-		vectors ( index_type => positive,
-				element_type => natural);
+	package pac_my_vector is new vectors (
+		index_type		=> positive,
+		element_type	=> natural);
 
- 	v : type_my_vector.vector;
+ 	v : pac_my_vector.vector;
  	n : natural;
 begin
- 	type_my_vector.append(v,7); -- add first object
- 	type_my_vector.append(v,9); -- add next object
+ 	pac_my_vector.append (v,7); -- add first object
+ 	pac_my_vector.append (v,9); -- add next object
 
- 	n := type_my_vector.element(v,2); -- get object from pos. 2
- 	put_line(natural'image(n));
+ 	n := pac_my_vector.element (v,2); -- get object from pos. 2
+ 	put_line(natural'image (n));
 	
 end cont_vectors_1;
